959b99fdffbdced97a677dc783ef7f2d511b60ed
Soumya Kundu <soumya.kundu@pnnl.gov>
2020-03-27 16:00:15 -0700

    I have modified the makefile copy commands to save two results csv files - one with the selected outputs, and one with all the outputs. I have also modified the interface.jl file with addition of a "devInfo" structure to store the device specific information, such as baseline (normal) usage pattern, comfort coefficient (alpha), associated sensor measurements, etc.

--- control/interface.jl
+++ control/interface.jl
@@ -16,8 +16,8 @@ using .con
 # ---------------
 # Set URL for testcase
 url = "http://emulator:5000"
-length = 1200#86400
-step = 600
+simLength_inSec = 120;#7*24*60*60;     # in [s]
+simStep_inSec   = 60;              # in [s]
 # ---------------
 
 # GET TEST INFORMATION
@@ -34,6 +34,19 @@ inputs = JSON.parse(String(HTTP.get("$url/inputs").body))
 measurements = JSON.parse(String(HTTP.get("$url/measurements").body))
 #println("Measurements:\t\t\t$measurements")
 
+# define a struct variable to contain the device information
+mutable struct devInfo
+    devType::String
+    # powRated::Any      # [kW]
+    devParam::Any          # [OPTIONAL] only if needed for calculation of scores
+    senseKey::Any
+    senseVal::Any
+    senseIdx::Any
+    transProb::Any
+    allStates::Any
+    uniqueSeq::Any
+end
+
 # Default simulation step
 step_def = JSON.parse(String(HTTP.get("$url/step").body))
 println("Default Simulation Step:\t$step_def")
@@ -46,14 +59,23 @@ CSV.write("measurements.csv", sort!(df))
 
 # specify the zone(s) and floor(s) you are interested in
 
+typeDev  = ["PC","Light","Misc"];
+
+dlen     = length(typeDev);
+allDev   = Array{devInfo}(undef,dlen);
+
 floor_idx = 1;
 zone_idx = 4;
 
 u_of_interest = inputs[occursin.("floor$(floor_idx)_zon$(zone_idx)",inputs)];
 y_of_interest = measurements[occursin.("floor$(floor_idx)_zon$(zone_idx)",measurements)];
 
-# println("Control Inputs:\t\t\t$u_of_interest")
-# println("Measurements:\t\t\t$y_of_interest")
+for iDev = 1:dlen
+   allDev[iDev] = devInfo(typeDev[iDev],undef,undef,undef,undef,undef,undef,undef);
+   allDev[iDev].senseKey = string("pow",typeDev[iDev][1:min(3,length(typeDev[iDev]))]);
+   allDev[iDev].senseIdx = findfirst(x->occursin(Regex(allDev[iDev].senseKey,"i"),x),y_of_interest);
+   allDev[iDev].devParam = [2.3, rand(Int64(24*60*60/simStep_inSec)), 0.9];  # comfort paramter ALPHA, usage probabilities
+end
 
 df = DataFrame(select_control_inputs=u_of_interest)
 CSV.write("select_control_inputs.csv", sort!(df))
@@ -65,26 +87,51 @@ CSV.write("select_measurements.csv", sort!(df))
 start_test = Dates.now()
 # Reset test case
 println("Resetting test case if needed.")
-start=86400*200
+start = 0;#86400*200
 res = HTTP.put("$url/reset",["Content-Type" => "application/json"], JSON.json(Dict("start" => start)))
 println("Running test case ...")
 # Set simulation step
-println("Setting simulation step to $step")
-res = HTTP.put("$url/step",["Content-Type" => "application/json"], JSON.json(Dict("step" => step)))
+println("Setting simulation step to $simStep_inSec")
+res = HTTP.put("$url/step",["Content-Type" => "application/json"], JSON.json(Dict("step" => simStep_inSec)))
+
+# create the variables arrays to be filled witth time-series data
+tlen = convert(Int, floor(simLength_inSec/simStep_inSec));
+ylen = length(y_of_interest);
+
+tSeries  = Array{Any}(undef,tlen,1); # stores the time-indices to pull out sampled data
+time_dd  = Array{Any}(undef,tlen,1); # time-indices in DAYS
+time_hh  = Array{Any}(undef,tlen,1); # time-indices in HOURS
+time_mm  = Array{Any}(undef,tlen,1); # time-indices in MINUTES
+ySeries  = Array{Any}(undef,tlen,ylen);
+allSeries = Array{Any}(undef,tlen,length(measurements)-1); # -1 because "time" is separate
 
 # simulation loop
-for i = 1:convert(Int, floor(length/step))
-    if i<2
-    # Initialize u
-       u = con.initialize()
-    else
-    # Compute next control signal
-       u = con.compute_control(y)
-    end
-    # Advance in simulation
-    res = HTTP.post("$url/advance", ["Content-Type" => "application/json"], JSON.json(u);retry_non_idempotent=true).body
-    global y = JSON.parse(String(res))
+for i = 1:tlen
+   if i<2
+   # Initialize u
+      u = con.initialize()
+   else
+   # Compute next control signal
+      u = con.compute_control(y)
+   end
+   # Advance in simulation
+   res = HTTP.post("$url/advance", ["Content-Type" => "application/json"], JSON.json(u);retry_non_idempotent=true).body
+   global y = JSON.parse(String(res));
+
+   tSeries[i] = y["time"];
+   ySeries[i,:] = [y[name_y] for name_y in y_of_interest];
+   allSeries[i,:] = [y[name_y] for name_y in setdiff(measurements,["time"])];
+   if rem(i,min(1,floor(tlen/20)))==0
+      println("$(Int64(round(i/tlen*100)))% complete ...")
+   end
+
+   # convert time into dd-hh-mm-ss format
+   time_dd[i] = convert(Int64,ceil(tSeries[i]/24/3600));
+   time_hh[i] = convert(Int64,ceil((tSeries[i]-(time_dd[i]-1)*24*3600)/3600));
+   time_mm[i] = convert(Int64,ceil((tSeries[i]-(time_dd[i]-1)*24*3600-(time_hh[i]-1)*3600)/60));
 end
+occIdx      = findfirst(x->occursin(r"OccSch"i,x),y_of_interest);
+occSeries   = ySeries[:,occIdx];
 
 println("Test case complete.")
 time=(Dates.now()-start_test).value/1000.
@@ -93,32 +140,51 @@ println("Elapsed time of test was $time seconds.")
 # --------------------
 # POST PROCESS RESULTS
 # --------------------
-# Get result data
-res = JSON.parse(String(HTTP.get("$url/results").body))
-
-time = [x/1.0 for x in res["y"]["time"]]; # in [s]
-
-# convert time into dd-hh-mm-ss format
-time_dd = convert.(Int64,ceil.(time/24/3600));
-time_hh = convert.(Int64,ceil.((time-(time_dd.-1)*24*3600)/3600));
-time_mm = convert.(Int64,ceil.((time-(time_dd.-1)*24*3600-(time_hh.-1)*3600)/60));
-time_ss = convert.(Int64,ceil.(time-(time_dd.-1)*24*3600-(time_hh.-1)*3600-(time_mm.-1)*60));
 
 # add whatever variables you want to add
-ylen = size(y_of_interest)[1];
-colnames    = Array{String}(undef,4+ylen);
-colvals     = Array{Any}(undef,4+ylen);
-colnames[1:4] = ["dd","hh","mm","ss"];    # adding column names
-colvals[1:4]  = [time_dd,time_hh,time_mm,time_ss];
-for iy = 1:ylen
-   colvals[4+iy]  = res["y"][y_of_interest[iy]]; # pick the iy-th output
-   colnames[4+iy] = y_of_interest[iy];
+colname    = Array{String}(undef,3+ylen+dlen);
+colvals    = Array{Any}(undef,tlen,3+ylen+dlen);
+
+colname[1:3] = ["dd","hh","mm"];    # adding column names
+colvals[:,1] = time_dd;
+colvals[:,2] = time_hh;
+colvals[:,3] = time_mm;
+
+colname[4:3+ylen]    = y_of_interest;
+colvals[:,4:3+ylen]  = ySeries;
+
+for id = 1:dlen
+   colname[3+ylen+id] = string("cmf_$(allDev[id].devType)");
+   devUse = Float64.(ySeries[:,allDev[id].senseIdx].>1e-6);
+   cmfAlp = allDev[id].devParam[1];
+   dayUse = allDev[id].devParam[2];
+   colvals[:,3+ylen+id] = (exp.(cmfAlp * (1 .- abs.(devUse - dayUse[Int64.(floor.((time_hh*60+time_mm)*60/simStep_inSec))]) )) .- 1)/(exp(cmfAlp)-1);
 end
+# colname[end]   = y_of_interest[occIdx];
+# colvals[:,end] = occSeries;
+
 tab = DataFrame(colvals);
 
-# names!(tab, Symbol.(colnames))
-rename!(tab, Symbol.(colnames))
+rename!(tab, Symbol.(colname), makeunique=true)
 CSV.write("result_testcase2.csv",tab)
+
+
+# this table stores the time-series of ALL the variables
+colname    = Array{String}(undef,2+length(measurements));
+colvals    = Array{Any}(undef,tlen,2+length(measurements));
+
+colname[1:3] = ["dd","hh","mm"];    # adding column names
+colvals[:,1] = time_dd;
+colvals[:,2] = time_hh;
+colvals[:,3] = time_mm;
+
+colname[4:end] = setdiff(measurements,["time"]);
+colvals[:,4:end]  = allSeries;
+
+tab = DataFrame(colvals);
+
+rename!(tab, Symbol.(colname), makeunique=true)
+CSV.write("all_result_testcase2.csv",tab)
 
 # stop the emulator
 HTTP.put("$url/stop")
--- makefile
+++ makefile
@@ -13,8 +13,10 @@ update:
 	cd ../../..
 
 copy:
-	docker cp c3po_control_1:/usr/myapp/result_testcase2.csv results/result.csv
+	mkdir -p results
+	docker cp c3po_control_1:/usr/myapp/all_result_testcase2.csv results/all_result.csv
+	docker cp c3po_control_1:/usr/myapp/result_testcase2.csv results/zone_result.csv
 	docker cp c3po_control_1:/usr/myapp/control_inputs.csv results/control_inputs.csv
 	docker cp c3po_control_1:/usr/myapp/measurements.csv results/measurements.csv
-	docker cp c3po_control_1:/usr/myapp/select_control_inputs.csv results/select_control_inputs.csv
-	docker cp c3po_control_1:/usr/myapp/select_measurements.csv results/select_measurements.csv
+	docker cp c3po_control_1:/usr/myapp/select_control_inputs.csv results/zone_control_inputs.csv
+	docker cp c3po_control_1:/usr/myapp/select_measurements.csv results/zone_measurements.csv
