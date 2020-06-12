6d3259586445fd13a6a9ea5ceb93ad8ae632b5eb
Huang, Sen <huang875@pnnl.gov>
2020-06-12 16:17:39 -0700

    clean up based on the recent update

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-# basic docker commends to manage the life-cycle of a docker image.
+# basic makefile commends to manage the life-cycle of a docker image.
 
 build:
 	docker-compose -f docker-compose_emulator.yml -f docker-compose_python.yml build
@@ -6,19 +6,14 @@ build:
 run:
 	docker-compose -f docker-compose_emulator.yml -f docker-compose_python.yml up
 	
-run_emulator:
-	docker-compose -f docker-compose_emulator.yml up	
-
-remove:
-	docker-compose -f docker-compose_emulator.yml -f docker-compose_python.yml down
-
-# docker-compose_python.yml will execute runSimulation.py
-# the   # GET TEST INFORMATION section will complile the lists of measurements, control inputs, and simulation results
-# to extract the results from docker use
 copy:
 	docker cp python:/usr/src/app/controlInputsList.csv 	interfaceLists/controlInputsList.csv
 	docker cp python:/usr/src/app/measurementsList.csv 		interfaceLists/measurementsList.csv
 	docker cp python:/usr/src/app/results.csv 						results/results.csv
 	docker cp python:/usr/src/app/infos.json 						results/infos.json
 
+remove:
+	docker-compose -f docker-compose_emulator.yml -f docker-compose_python.yml down
 
+run_emulator:
+	docker-compose -f docker-compose_emulator.yml up	
--- python_control/client_examples.py
+++ python_control/client_examples.py
@@ -1,11 +1,13 @@
 """
-.. module:: runSimulation
+
+.. module:: client_examples
 
   :platform: Unix, Windows
 
   :synopsis: This module tests starting and running a simulation within the JModelica docker.
 
-.. moduleauthor:: PNNL
+.. author:: PNNL
+
 """
 # -*- coding: utf-8 -*-
 # GENERAL PACKAGE IMPORT
@@ -16,7 +18,6 @@ import os
 import csv
 import time
 import urllib
-import json
 
 # ----------------------
 
@@ -28,6 +29,7 @@ def ctrlInitialize(inputs):
     elif "_activate" in inputs[ind]:
       u[inputs[ind]] = 0
   return u
+
 
 def main(argv):
   # SETUP SIMULATION
@@ -100,7 +102,6 @@ def main(argv):
       writer = csv.writer(outFile)
       for line in measurements:
         writer.writerow([line])
-        
   outFileName = "results.csv"
   if os.path.exists(outFileName):
     os.remove(outFileName)
@@ -132,11 +133,99 @@ def main(argv):
   while timeStep <= int(simDuration/fmuStep):
     # Advance simulation
     y = requests.post('{0}/advance'.format(url), data = u).json()
-    # print(sorted(y.items(), key = lambda x: x[0]))
+
+    # y holds data output from the emulator
 
     # Compute next control signal
     #----------
     # Put your code here to change control signals
+
+    # ----------
+    # Example 1
+    # ----------
+    # Injection of bias sensor fault
+    # ----------
+    bias_example = True # Flag to run bias injection example
+    if(bias_example):
+      # Select bias amount and sensor location
+      bias_degK = 2 # bias temp amount (in degree Kelvin)
+      bias_floor = 3 # floor # to apply bias
+      bias_zone = 5 # zone # to apply bias
+
+      # Define variable names for Air Temperature of selected floor and zone
+      Output_AirTemp_varname = "floor{0}_zon{1}_TRooAir_y".format(bias_floor, bias_zone) # from Output Measurements
+      ctrlInput_AirTemp_varname = "floor{0}_zon{1}_oveTRooAir_u".format(bias_floor, bias_zone)  # from Output Measurements
+      Activation_AirTemp_varname = "floor{0}_zon{1}_oveTRooAir_activate".format(bias_floor, bias_zone)
+
+      # Measure true sensor value output Air Temperature of selected floor and zone
+      output_AirTemp = y[Output_AirTemp_varname]
+
+      # Add bias Air Temperature measurement
+      biased_AirTemp = output_AirTemp + bias_degK
+
+      # Check impact of override from previous simulation step
+      print("Example 1 Status: Bias Air Temp Sensor Fault")
+      print("--Input Air Temp = {0}".format(u[ctrlInput_AirTemp_varname]))
+      print("--Output Measured Air Temp before Bias = {0}".format(output_AirTemp))
+      print("--Output Measured Air Temp after Bias = {0}".format(biased_AirTemp))
+
+      # Write biased value to the control input of Air Temperature of selected floor and zone
+      u[ctrlInput_AirTemp_varname] = biased_AirTemp
+
+      # Activate Air Temperature measurement of selected floor and zone
+      u[Activation_AirTemp_varname] = 1
+
+      # Updated u is passed to the emulator through the "advance" call above
+
+    # ----------
+    # End Example 1
+    # Continue to increment simulation step
+    # ----------
+    # ----------
+
+
+    # ----------
+    # Example 2
+    # ----------
+    # Change AHU supply air temperature Set Point
+    # ----------
+    override_example = True
+    if(override_example):
+
+      # Select change location
+      override_floor = 3 # floor # to apply change
+
+      # Variable name for
+      Output_AHUdischargeAirTempSetPt_varname = "floor{0}_conCoiEco_TSupSetPoiSig_act_y".format(override_floor)
+      Output_AHUdischargeAirTemp_varname = "floor{0}_conCoiEco_TSupRead_y".format(override_floor)
+      ctrlInput_AHUsupplyAirTemp_varname = "floor{0}_conCoiEco_TSupSetPoiSig_ove_u".format(override_floor)
+      Activation_AHUsupplyAirTemp_varname = "floor{0}_conCoiEco_TSupSetPoiSig_ove_activate".format(override_floor)
+
+      # Measure AHU discharge Air Temp Set Point of selected floor
+      output_dischargeAirTempSetPt = y[Output_AHUdischargeAirTempSetPt_varname]
+
+      # Measure AHU discharge Air Temp of selected floor
+      output_dischargeAirTemp = y[Output_AHUdischargeAirTemp_varname]
+
+      # Check impact of override from previous simulation step
+      print("Example 2 Status: Overriding AHU Air Temp")
+      print("--Input AHU supply Air Temp Set Point = {0}".format(u[ctrlInput_AHUsupplyAirTemp_varname]))
+      print("--Output AHU discharge Air Temp Set Point = {0}".format(output_dischargeAirTempSetPt))
+      print("--Output Measured AHU discharge Air Temp = {0}".format(output_dischargeAirTemp))
+
+      # Override AHU discharge Air Temp Set Point of selected floor to the control input
+      u[ctrlInput_AHUsupplyAirTemp_varname] = output_dischargeAirTempSetPt
+
+      # Activate Air Temperature measurement of selected floor and zone
+      u[Activation_AHUsupplyAirTemp_varname] = 1
+
+      # Updated u is passed to the emulator through the "advance" call above
+
+    # ----------
+    # End Example 2
+    # Continue to increment simulation step
+    # ----------
+    # ----------
 
     #----------
     # u = ins
